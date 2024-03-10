77c1a42a8c729ec4865c209f2b7a6fb97ab0614c
pejman <pejman.hkh@gmail.com>
2024-03-10 11:13:38 +0330

    add inclass and getattr to attr

--- gdp/attr.go
+++ gdp/attr.go
@@ -1,6 +1,29 @@
 package gdp
 
+import "strings"
+
 type Attr struct {
 	name  string
 	value string
+}
+
+func getAttrIndex(attrs []Attr, index string) Attr {
+	var ret Attr
+	for _, attr := range attrs {
+		if attr.name == index {
+			ret = attr
+			break
+		}
+	}
+	return ret
+}
+
+func (a *Attr) inClass(v string) bool {
+	split := strings.Split(a.value, " ")
+	for _, s := range split {
+		if s == v {
+			return true
+		}
+	}
+	return false
 }
