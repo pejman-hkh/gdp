7abd6b4ed855df55ce56019b505c776d8b7415f2
pejman <pejman.hkh@gmail.com>
2024-03-10 11:12:16 +0330

    query parsing

---
+++ gdp/query.go
@@ -0,0 +1,95 @@
+package gdp
+
+type Query struct {
+	query string
+	i     int
+	len   int
+}
+
+type QueryAttr struct {
+	query string
+	i     int
+	len   int
+}
+
+func (q *Query) parseQuery(splited *string) bool {
+	if q.i == q.len {
+		return false
+	}
+
+	c := q.query[q.i]
+
+	if c == ' ' || c == ',' || c == '>' {
+		q.i++
+		*splited = string(c)
+		return true
+	}
+
+	a := ""
+	for {
+		c := q.query[q.i]
+		q.i++
+
+		if c == ' ' || c == ',' || c == '>' {
+			q.i--
+			break
+		}
+		a += string(c)
+
+		if q.i == q.len {
+			break
+		}
+	}
+	*splited = a
+	return true
+}
+
+func (q *QueryAttr) getAttr() string {
+	a := ""
+	for {
+		if q.i == q.len {
+			break
+		}
+		c := q.query[q.i]
+		q.i++
+
+		if c == '#' || c == '.' || c == '[' || c == '=' || c == ']' {
+			break
+		}
+		a += string(c)
+	}
+	q.i--
+	return a
+}
+
+func (q *QueryAttr) parseAttr() map[string]string {
+	ret := make(map[string]string)
+
+	for {
+		c := q.query[q.i]
+		q.i++
+		if q.i == q.len {
+			break
+		}
+
+		if c == '.' {
+			ret["class"] = q.getAttr()
+		} else if c == '#' {
+			ret["id"] = q.getAttr()
+
+		} else if c == ']' {
+			q.i++
+		} else if c == '[' {
+
+			key := q.getAttr()
+			q.i++
+
+			ret[key] = q.getAttr()
+		} else {
+			q.i--
+			ret["tag"] = q.getAttr()
+		}
+	}
+
+	return ret
+}
