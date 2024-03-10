d0909c767c323def607d631e37e1527ac9e8edb8
pejman <pejman.hkh@gmail.com>
2024-03-10 18:37:36 +0330

    change attr array as attr array pointer

--- gdp/attr.go
+++ gdp/attr.go
@@ -1,24 +1,51 @@
 package gdp
 
-import "strings"
+import (
+	"strings"
+)
 
 type Attr struct {
 	name  string
 	value string
 }
 
-func getAttr(attrs []Attr, index string) Attr {
-	var ret Attr
+func makeAttr(attrs []*Attr) string {
+	ret := " "
+	pre := ""
+
 	for _, attr := range attrs {
+		value := attr.value
+		name := attr.name
+
+		if name == "class" && value == "" {
+			continue
+		}
+
+		ret += pre + name + `="` + value + `"`
+		pre = " "
+
+	}
+	return ret
+}
+
+func getAttr(attrs []*Attr, index string) *Attr {
+	var ret *Attr
+	for _, attr := range attrs {
+
 		if attr.name == index {
 			ret = attr
 			break
 		}
 	}
+
 	return ret
 }
 
 func (a *Attr) inClass(v string) bool {
+	if a == nil {
+		return false
+	}
+
 	split := strings.Split(a.value, " ")
 	for _, s := range split {
 		if s == v {
