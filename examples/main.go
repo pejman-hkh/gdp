a24611c7f6d7770fea7cdecfe6640dffeaf64756
pejman <pejman.hkh@gmail.com>
2024-03-11 19:25:55 +0330

    update example

--- examples/main.go
+++ examples/main.go
@@ -12,14 +12,14 @@ func main() {
 	fileContent, _ := os.ReadFile("fightclub.html")
 	document := gdp.Default(string(fileContent))
 	found := document.Find(".ipc-image")
-	fmt.Printf("%+v", found.Eq(0).Attr("src"))
+	fmt.Printf("%+v\n", found.Eq(0).Attr("src"))
 
 	document = gdp.Default(`<div class="parent"><div class="prev">test</div><div class="middle" id="middle">test1</div><span class="next"></span></div>`)
 
-	fmt.Printf("%+v", document)
+	fmt.Printf("%+v\n", document)
 
 	document.Find(".prev,.middle,.next").Each(func(index int, tag *gdp.Tag) {
-		fmt.Println(tag)
+		fmt.Printf("%+v\n", tag)
 	})
 
 	middle := document.GetElementById("middle")
