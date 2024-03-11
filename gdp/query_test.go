16609bf394bb5774b8cece3b2833610c5ec509e0
pejman <pejman.hkh@gmail.com>
2024-03-11 14:33:57 +0330

    add extra test for query

--- gdp/query_test.go
+++ gdp/query_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestTag(t *testing.T) {
+func TestQuery(t *testing.T) {
 
 	p := splitQuery("a")
 	got := p[0]
@@ -14,9 +14,17 @@ func TestTag(t *testing.T) {
 		t.Errorf("got %q, wanted %q", got, want)
 	}
 
+	p = splitQuery("a.link")
+	got = p[0]
+	want = "a.link"
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+
 }
 
-func TestSplitQuery(t *testing.T) {
+func TestSplitQueries(t *testing.T) {
 	a := splitQueries(".test a, .test1 a")
 	got := a[0]
 	want := ".test a"
@@ -29,4 +37,31 @@ func TestSplitQuery(t *testing.T) {
 	if got != want {
 		t.Errorf("got %q, wanted %q", got, want)
 	}
+}
+
+func TestAtt(t *testing.T) {
+	q := "a[href='test'][class='aa']"
+	qa := QueryAttr{q, 0, len(q)}
+	attrs := qa.parseAttr()
+
+	got := attrs["tag"]
+	want := "a"
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+
+	got = attrs["href"]
+	want = "test"
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+
+	got = attrs["class"]
+	want = "aa"
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+
 }
