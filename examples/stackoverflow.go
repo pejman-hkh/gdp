4387d3344227de3527d28cf4696da99df19cab4e
pejman <pejman.hkh@gmail.com>
2024-03-11 12:20:33 +0330

    update

--- examples/stackoverflow.go
+++ examples/stackoverflow.go
@@ -11,8 +11,6 @@ func main() {
 	html, _ := os.ReadFile("stackoverflow.html")
 
 	document := gdp.Default(string(html))
-	//fmt.Print(document.Html())
-	//document.Print()
 
 	document.Find("#tags-browser .grid--item").Each(func(index int, tag *gdp.Tag) {
 		link := tag.Find("a").Eq(0)
