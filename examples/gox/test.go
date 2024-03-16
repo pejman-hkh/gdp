372094857319e0524df3d45a7cda10a4fd8430d9
pejman <pejman.hkh@gmail.com>
2024-03-16 18:10:15 +0330

    test gox with http

--- examples/gox/test.go
+++ examples/gox/test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"net/http"
 	"github.com/pejman-hkh/gdp/gox"
 )
 
@@ -39,25 +41,47 @@ func (r React) Footer(props map[string]string, childrens string) string {
 }
 
 var react gox.Gox = gox.Gox{React{}}
-func layout() string {
+func (r React) Layout(props map[string]string, childrens string) string {
 	return react.Run("f", map[string]string{}, []string{`
 	`, react.Run("html", map[string]string{}, []string{`
 	`, react.Run("head", map[string]string{}, []string{`
 	`}), `
 	`, react.Run("Header", map[string]string{`title` :`test`}, []string{`test 
-		`, react.Run("Link", map[string]string{`to` :`https://www.google.com/`}, []string{`Google`}), `
+		`, react.Run("Link", map[string]string{`to` :`/about`}, []string{`About`}), `
+		`, react.Run("Link", map[string]string{`to` :`/contact`}, []string{`About`}), `
 	`}), `
 	`, react.Run("SideNav", map[string]string{}, []string{`
-		`, react.Run("li", map[string]string{}, []string{react.Run("a", map[string]string{`href` :`https://github.com`}, []string{`Test`})}), `
+		`, react.Run("li", map[string]string{}, []string{react.Run("a", map[string]string{`href` :`/contact`}, []string{`Contact`})}), `
 	`}), `
-	`, react.Run("Content", map[string]string{}, []string{`
-		main content
-	`}), `
+	`, react.Run("Content", map[string]string{}, []string{`		`,childrens}), `
 	`, react.Run("Footer", map[string]string{`title` :`test`}, []string{`test`}), `
 	`}), `
 	`})
 }
 
+func routes(w http.ResponseWriter, req *http.Request) {
+	path := req.URL.Path
+	route := strings.Split(path, "/")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	if route[1] == "home" {
+		fmt.Fprint(w, react.Run("Layout", map[string]string{}, []string{`
+		Home Page
+		`}))
+	} else if route[1] == "about" {
+		fmt.Fprint(w, react.Run("Layout", map[string]string{}, []string{`
+		About
+		`}))
+	} else if route[1] == "contact" {
+		fmt.Fprint(w, react.Run("Layout", map[string]string{}, []string{`
+		Contact
+		`}))
+	}
+}
+
 func main() {
-	fmt.Print(layout())
+
+	http.HandleFunc("/", routes)
+	http.ListenAndServe(":8090", nil)
+
 }
